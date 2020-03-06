98b774c251e4310d33197230a3fe83e7e1e51ef1
Roshel Samarasinghe <shellzzonez@gmail.com>
2020-03-06 16:15:44 +0000

    made basic calculator script

---
+++ Calculator2 .php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action="Calculator.php" method="post">
+
+Number 1 <input type="number" name="num1"> <br> <br>
+
+Operator: <input type="textbox" name="Operator"> <br><br>
+
+Number 2 <input type="number" name="num2"> <br> <br>
+
+
+
+
+
+</form>
+    
+    <?php
+
+//function sdsdsds ($f){}
+
+function calc($num1,$num2){
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["Operator"];
+
+    if ($op=="+"){
+        echo $num1 + $num2;
+    } elseif($op=="-"){
+        echo $num1 - $num2;
+    } elseif($op=="*"){
+        echo $num1 * $num2;
+    } elseif ($op=="/"){
+        echo $num1 / $num2;
+    } else {
+        echo "invalid";
+    }
+
+}
+
+// if statements are good to narrow down options
+
+?>
+
+
+
+</body>
+</html>
